58c0a09af7a3b7c8941f897568589fdd990b3311
root <root@Chris-Computer>
2024-07-15 13:59:31 +0100

    latest changes

--- app.py
+++ app.py
@@ -179,3 +179,5 @@ if __name__ == '__main__':
     # Create database tables if they don't exist
     db.create_all()
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
